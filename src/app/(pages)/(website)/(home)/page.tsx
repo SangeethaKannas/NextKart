7faac8bc8dd745f96546eafbd1116ee16a3857c2
Sangeetha Kanna S <sangeethakannas@gmail.com>
2025-01-02 15:28:42 +0530

    Added Eslint and prettier

--- src/app/(pages)/(website)/(home)/page.tsx
+++ src/app/(pages)/(website)/(home)/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div>
       Home
+      <Image src="/assets/images/nextkart.png" width="200" height="100" alt="Next Kart Logo" />
+
     </div>
   );
 }
